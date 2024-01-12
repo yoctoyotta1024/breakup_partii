d7506e50aea74de0bab78edb1d4dc1de56e351d4
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-12 09:47:03 +0100

    edit config params in run script

--- src/run_buii.py
+++ src/run_buii.py
@@ -28,16 +28,16 @@ from matplotlib.colors import LogNorm, Normalize
 path2CLEO = sys.argv[1]
 path2build = sys.argv[2]
 tmp_configfile = sys.argv[3]
-tmpdir = path2build+"/tmp/"
 
 sys.path.append(path2CLEO)  # for imports from pySD package
-from pySD.initsuperdropsbinary_src import *
-from pySD.initsuperdropsbinary_src import create_initsuperdrops as csupers 
-from pySD.initsuperdropsbinary_src import read_initsuperdrops as rsupers 
+from pySD import editconfigfile
 
 ### ---------------------------------------------------------------- ###
 ### ----------------------- INPUT PARAMETERS ----------------------- ###
 ### ---------------------------------------------------------------- ###
+# labels for model compilations
+labels = ["coalbure", "coalonly"]
+
 # name of executables and data output
 executables = {
   "coalbure" : "buii_coalbure",
@@ -51,22 +51,36 @@ executables = {
   "coalonly" : runnums
 }
 
+binpath       = path2build+"/bin/"
+sharepath     = path2build+"/share/"
+tmppath       = path2build+"/tmp/"
+initSDspath   = sharepath+"/buii_dimlessSDsinits/"
 ### ---------------------------------------------------------------- ###
 ### ---------------------------- RUN CLEO -------------------------- ###
 ### ---------------------------------------------------------------- ###
 if path2CLEO == path2build:
   raise ValueError("build directory cannot be CLEO")
-else:
 
-  os.chdir(path2build)
-  os.system('pwd')
-  for exec in executables:
-    executable = path2build+'/src/'+exec
+for n in runnums:
+  runstr = "run"+str(n)
 
-    for n in runnums:
-      runstr = "run"+str(n)
-      
-      configfile = 
-      os.system(executable + ' ' + configfile)
+  for lab in labels:
+  
+    ### ----- copy tmp_config to config then edit ----- ###
+    configfile = tmppath+"/config_"+lab+"_"+runstr+".txt"
+    os.system('cp '+tmp_configfile+" "+configfile)
+    params = {
+      "initsupers_filename" : initSDspath+"/"+runstr+".dat",
+      "setuptxt" : binpath+"/"+lab+"/setup_"+runstr+".txt",
+      "zarrbasedir" : binpath+"/"+lab+"/sol_"+runstr+".zarr",
+      "stats_filename" : binpath+"/"+lab+"/stats_"+runstr+".txt",
+      }
+    editconfigfile.edit_config_params(configfile, params)
+
+    ### ----- run executabel with new configfile ----- ### 
+    os.chdir(path2build)
+    os.system('pwd')
+    executable = path2build+'/src/'+executables[lab]
+    os.system(executable + ' ' + configfile)
 ### ---------------------------------------------------------------- ###
 ### ---------------------------------------------------------------- ###
