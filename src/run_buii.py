8afba7be6a082c4a4a189e2eaa87a0a250b3bda4
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-12 01:23:44 +0100

    move initSDs into sep python file

--- src/run_buii.py
+++ src/run_buii.py
@@ -44,27 +44,6 @@ constsfile    = path2CLEO+"/libs/cleoconstants.hpp"
 sharepath     = path2build+"/share/"
 gridfile      = sharepath+"buii_dimlessGBxboundaries.dat"
 initSDsfile   = sharepath+"buii_dimlessSDsinit.dat"
-
-### --- plotting initialisation figures --- ###
-isfigures   = [True, True] # booleans for [making, saving] initialisation figures
-savefigpath = path2build+"/bin/" # directory for saving init figures
-SDgbxs2plt  = list(range(39, 99))
-SDgbxs2plt  = [random.choice(SDgbxs2plt)] # choose random gbx from list to plot 
-
-### --- settings for initial superdroplets --- ###
-# initial superdroplet coordinates
-zlim        = 800       # min z coord of superdroplets [m]
-npergbx     = 256       # number of superdroplets per gridbox 
-
-# initial superdroplet radii (and implicitly solute masses)
-rspan       = [3e-9, 5e-5]                      # min and max range of radii to sample [m]
-dryr_sf     = 1.0                               # dryradii are 1/sf of radii [m]
-
-# settings for initial superdroplet multiplicies
-geomeans             = [0.02e-6, 0.2e-6, 3.5e-6]               
-geosigs              = [1.55, 2.3, 2]                    
-scalefacs            = [1e6, 0.3e6, 0.025e6]   
-numconc = np.sum(scalefacs) * 100
 ### ---------------------------------------------------------------- ###
 ### ---------------------------------------------------------------- ###
 
