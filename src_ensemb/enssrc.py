873a533de80aa6f3bb79f501f7966c094439cf4c
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-20 18:10:52 +0100

    numconc distrib calculated

--- src_ensemb/enssrc.py
+++ src_ensemb/enssrc.py
@@ -28,6 +28,43 @@ sys.path.append("/home/m/m300950/CLEO/")  # path2CLEO for imports from pySD pack
 from pySD.sdmout_src import *             # pyzarr, pysetuptxt & pygbxsdat
 import pySD.sdmout_src.ensembzarr as enszarr
 
+def get_log10redgs(rspan, nbins):
+  ''' returns edges of log10(r) bins for 'nbins'
+  evenly spaced from rspan[0] to rspan[1]'''
+
+  log10redgs = np.linspace(np.log10(rspan[0]),
+                             np.log10(rspan[1]),
+                             nbins+1)  
+  
+  return log10redgs
+
+def log10r_histogram(log10redgs, log10radius, wghts):
+  ''' returns (weighted) frequency in each log(r) bin
+  with edges given by log10r_hedges'''
+
+   # get (weighted) number frequency in each bin
+  return np.histogram(log10radius, bins=log10redgs,
+                      weights=wghts, density=None)[0]
+
+def log10r_distrib(rspan, nbins, radius, wghts, perlog10r=False):
+  ''' get distribution of data with weights 'wghts' against
+  log10(r). Uses np.histogram to get frequency of a particular
+  value of data that falls into bins evenly spaced in log10(r) '''
+
+  # create edges of log10(r) histogram bins (evenly spaced in log10(r))
+  log10redgs = get_log10redgs(rspan, nbins) 
+  log10r = np.log10(radius)
+  hist = log10r_histogram(log10redgs, log10r, wghts)
+
+  if perlog10r == True: # histogram frequency / delta_log10(r)
+    log10rwdths = log10redgs[1:]- log10redgs[:-1]                 # ln10(r) histogram bin widths
+    hist = hist/log10rwdths 
+ 
+  redges = 10**log10redgs                                             # radius edges of bins
+  rcens = (10**(log10redgs[1:]) + 10**(log10redgs[:-1])) / 2        # radius centres of bins
+
+  return hist, redges, rcens # units of hedgs and hcens = units of rspan (usually [microns])
+
 def write_ensemble_domaindists(ensembdataset, ensembsetupfile,
                                setupfile, gridfile, datasets,
                                distparams):
@@ -70,46 +107,28 @@ def ensemble_domainnumconc_distrib(ensembdataset,
   
   ensemble_distrib()
 
-def log10r_distribution(rspan, nbins, radius, wghts, perlog10r=False):
-  ''' get distribution of data with weights 'wghts' against
-  log10(r). Uses np.histogram to get frequency of a particular
-  value of data that falls into bins evenly spaced in log10(r) '''
-
-  # create edges of log10(r) histogram bins (evenly spaced in log10(r))
-  log10r_hedgs = np.linspace(np.log10(rspan[0]), np.log10(rspan[1]), nbins+1)  
-  
-  # get (weighted) number frequency in each bin
-  hist = np.histogram(np.log10(radius), bins=log10r_hedgs,
-                      weights=wghts, density=None)[0]
-
-  if perlog10r == True: # histogram frequency / delta_log10(r)
-    log10r_hwdths = log10r_hedgs[1:]- log10r_hedgs[:-1]                 # ln10(r) histogram bin widths
-    hist = hist/log10r_hwdths
-
-  redges = 10**log10r_hedgs                                             # radius edges of bins
-  rcens = (10**(log10r_hedgs[1:]) + 10**(log10r_hedgs[:-1])) / 2        # radius centres of bins
-
-  return hist, redges, rcens # units of hedgs and hcens = units of rspan (usually [microns])
-
 def numconc_distrib(dataset, vol, gbxidx, nbins, rspan):
   '''calculate the real droplet number concentration
   for a gridbox with volume 'vol' and index 'gbxidx'.
   If gbxidx=="domain", all superdroplets in dataset
   are used, so 'vol' should be domain volume) '''
 
-  
+  log10redgs = get_log10redgs(rspan, nbins)
 
-  print("---- WIP -----")
   numconc = [] # array dims [time, nbins]
   if gbxidx == "domain":
+    radius = pyzarr.get_rawdata4raggedkey(dataset, "radius")
+    log10r = np.log10(radius)
     xi = pyzarr.get_rawdata4raggedkey(dataset, "xi")
 
-    for 
-  print(xi, len(xi), len(xi[0]))
-  # print(ak.shape(xi))
+    for t in range(len(radius)): # for each timestep
+      wghts = xi[t] / vol / 1e6          # real droplets [/cm^3]
+      hist = log10r_histogram(log10redgs, log10r[t], wghts)
+      numconc.append(hist)
   
+  numconc = np.asarray(numconc) # array dims [time, nbins]
   
-  print("---- WIP -----")
+  return numconc
 
 def ensemble_distrib():
 
