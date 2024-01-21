04a44c9197b9c00d32acd4e3f5fc3474eac1b1f4
Clara Levante <yoctoyotta1024@yahoo.com>
2024-01-21 15:45:35 +0100

    name change

--- src_ensemb/enssrc.py
+++ src_ensemb/enssrc.py
@@ -63,68 +63,49 @@ def write_ensemble_domaindists(ensembdataset, ensembsetupfile,
   for dataset in datasets:
     enszarr.check_dataset_for_ensemb(dataset, refset)
   
-  log10redgs = distcalcs.get_log10redgs(distparams["rspan"],
+  log10redges = distcalcs.get_log10redges(distparams["rspan"],
                               distparams["nbins"])
   
-  redges, rcens = distcalcs.get_redges_rcens(log10redgs)
+  redges, rcens = distcalcs.get_redges_rcens(log10redges)
   write_redges_rcens(ensembdataset, redges, rcens)
 
   write_ensemble_domainnumconc_distrib(ensembdataset,
                                        ensembsetupfile,
                                        gridfile,
                                        datasets,
-                                       log10redgs) 
+                                       log10redges) 
 
   write_ensemble_domainwatermass_distrib(ensembdataset,
                                        ensembsetupfile,
                                        gridfile,
                                        datasets,
-                                       log10redgs)  
+                                       log10redges)  
   
   write_ensemble_domainreflectivity_distrib(ensembdataset,
                                             datasets,
-                                            log10redgs) 
+                                            log10redges) 
 
 def write_domaindistrib_to_zarr(ensembdataset, name, meandist, stddist):
 
   print("TODO: write mean and std of "+name+" to zarr: "+ensembdataset)
-  
-def write_zarrarray(array, zarrarrayname, shape, 
-               chunks=(1250000), dtype='<f8'):
-  
-  z1 = zarr.open(zarrarrayname, mode='w', shape=shape,
-                 chunks=chunks, dtype=dtype, order='C',
-                 compressor=None, fill_value=None, 
-                 filters=None, zarr_format=2)
-
-  z1[:] = array
-
-def write_zattrs_metadata(arraydims, units=" ", scale_factor=1.0):
-
-  metadata = '{\n"_ARRAY_DIMENSIONS": '+arraydims+',\n'+\
-    '"units": '+units+',\n'+\
-      '"scale_factor": '+str(scale_factor)+\
-        '\n}'
-  print(metadata)
 
 def write_redges_rcens(ensembdataset, redges, rcens):
   
   zarrdataset = "/home/m/m300950/breakup_partii/plots/testzarr.zarr/" # TODO 
+ 
+  sf = 1e6 # scale_factor
+  units='micro m'
 
-  sf = 1e6
   arrayname = zarrdataset+"/redges"
-  write_zarrarray(redges/sf, arrayname, redges.shape)
+  write_zarrarray(redges/sf, arrayname, redges.shape, ["redges"],
+                  units=units, sf=sf)
 
   arrayname = zarrdataset+"/rcens"
-  write_zarrarray(rcens/sf, arrayname, rcens.shape)
-
-  write_zattrs_metadata('["rcens"]', units="micro m", scale_factor=sf)
-  write_zattrs_metadata('["redges"]', units="micro m", scale_factor=sf)
-
-  print("TODO: write redges rcens")
+  write_zarrarray(rcens/sf, arrayname, rcens.shape, ["rcens"],
+                  units=units, sf=sf)
 
 def calc_dists_for_ensemb(calc_distrib_func, datasets,
-                          log10redgs, args):
+                          log10redges, args):
   ''' take mean of real droplet number
   concentration distributions.
   parametrs for distirubtions given by
@@ -132,7 +113,7 @@ def calc_dists_for_ensemb(calc_distrib_func, datasets,
   
   dists = []
   for dataset in datasets:
-    dist = calc_distrib_func(dataset, log10redgs, *args)
+    dist = calc_distrib_func(dataset, log10redges, *args)
     dists.append(dist)
   dists = np.asarray(dists)
 
@@ -152,7 +133,7 @@ def ensemble_distrib_mean_std(ensemb_dists):
 def write_ensemble_domainnumconc_distrib(ensembdataset,
                                          ensembsetupfile,
                                          gridfile, datasets,
-                                         log10redgs):
+                                         log10redges):
   ''' write mean and standard deviation real droplet number
   concentration distributions for domain over datasets
   of ensemble to zarr 'ensembdataset' where bins of
@@ -160,7 +141,7 @@ def write_ensemble_domainnumconc_distrib(ensembdataset,
   
   domainvol = distcalcs.get_domainvol(ensembsetupfile, gridfile) # [m^3]
   numconc_dists = calc_dists_for_ensemb(distcalcs.numconc_distrib,
-                                        datasets, log10redgs,
+                                        datasets, log10redges,
                                         ["domain", domainvol])
   
   meandist, stddist = ensemble_distrib_mean_std(numconc_dists)
@@ -171,7 +152,7 @@ def write_ensemble_domainwatermass_distrib(ensembdataset,
                                            ensembsetupfile,
                                            gridfile,
                                            datasets,
-                                           log10redgs): 
+                                           log10redges): 
   ''' write mean and standard deviation of real droplet
   mass (as if water) distribution for domain over
   datasets of ensemble to zarr 'ensembdataset' where bins
@@ -179,7 +160,7 @@ def write_ensemble_domainwatermass_distrib(ensembdataset,
     
   domainvol = distcalcs.get_domainvol(ensembsetupfile, gridfile) # [m^3]
   watermass_dists = calc_dists_for_ensemb(distcalcs.watermass_distrib,
-                                        datasets, log10redgs,
+                                        datasets, log10redges,
                                         ["domain", domainvol])
   
   meandist, stddist = ensemble_distrib_mean_std(watermass_dists)
@@ -189,14 +170,14 @@ def write_ensemble_domainwatermass_distrib(ensembdataset,
   
 def write_ensemble_domainreflectivity_distrib(ensembdataset,
                                               datasets,
-                                              log10redgs):
+                                              log10redges):
   ''' write mean and standard deviation of real droplet
   reflectivity proxy (6th moment of radius) distribution
   for domain over datasets of ensemble to zarr 'ensembdataset'
   where bins of distribution are defined by log10redges [microns] '''
 
   refproxy_dists = calc_dists_for_ensemb(distcalcs.reflectproxy_distrib,
-                                          datasets, log10redgs,
+                                          datasets, log10redges,
                                           ["domain"])
   
   meandist, stddist = ensemble_distrib_mean_std(refproxy_dists)
